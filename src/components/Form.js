520b651f889c9db9c4d8ddd45886c6e0ba57abe5
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-07-18 10:05:54 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+//import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  /*function Form() {
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
 
-  return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+  function handleFirstNameChange(event){
+    setFirstName(event.target.value);
+  }
+  function handleLastNameChange(event){
+    setLastName(event.target.value);
+  }
+    return (
+      <form>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange}  value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+    );
+    */
+    function Form(props) {
+      return (
+        <form>
+          <input
+            type="text"
+            onChange={props.handleFirstNameChange}
+            value={props.firstName}
+          />
+          <input
+            type="text"
+            onChange={props.handleLastNameChange}
+            value={props.lastName}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
 
 export default Form;
